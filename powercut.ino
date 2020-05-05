9c3a951aadb9191168c891e5a930cef1d86cc65c
Johnny Jacob <johnnyjacob@gmail.com>
2020-05-05 22:28:37 +0530

    Added license header.

--- powercut.ino
+++ powercut.ino
@@ -1,3 +1,26 @@
+/* MIT License */
+
+/* Copyright (c) 2020 Johnny Jacob <johnnyjacob@gmail.com> */
+
+/* Permission is hereby granted, free of charge, to any person obtaining a copy */
+/* of this software and associated documentation files (the "Software"), to deal */
+/* in the Software without restriction, including without limitation the rights */
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell */
+/* copies of the Software, and to permit persons to whom the Software is */
+/* furnished to do so, subject to the following conditions: */
+
+/* The above copyright notice and this permission notice shall be included in all */
+/* copies or substantial portions of the Software. */
+
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR */
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, */
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE */
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER */
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, */
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE */
+/* SOFTWARE. */
+		  
+// Credits :
 // BY: Akshaya Niraula
 // ON: Nov 11, 2016
 // AT: http://www.embedded-lab.com/
@@ -39,6 +62,7 @@ void setup() {
 
   bool flag = false;
   client.setInsecure();
+
   for (int i=0; i<5; i++){
     int retval = client.connect(host, httpsPort);
     if (retval == 1) {
@@ -86,6 +110,5 @@ void loop() {
   
   // Post these information
   postData("Status", 1);
-  
   delay (dataPostDelay);
 }

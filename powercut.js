10b278dc4d2524e43c5e5584e54890dfcb3c0200
Johnny Jacob <johnnyjacob@gmail.com>
2020-07-21 07:57:34 +0530

    Filter data points with 0 minutes interval. Cleaned up unused code.

--- powercut.js
+++ powercut.js
@@ -13,13 +13,14 @@ function drawChart() {
     dataTable.addColumn({type: 'string', id: 'style', role: 'style' });
     dataTable.addColumn({type: 'date', id: 'Start'});
     dataTable.addColumn({type: 'date', id: 'End'});
+
     var powerCutEvents = [];
     Papa.parse("http://localhost:1337/data.csv", { 
         download: true,
         header: true,
         delimiter: ",",
         step: function(row) {
-            //console.log(row.data);
+
             var start = new Date (Date.parse(row.data['start']));
             var end = new Date (Date.parse(row.data['end']));
 
@@ -29,7 +30,9 @@ function drawChart() {
             if (momentStart.isSame(momentEnd, 'days')) {
                 var startTime = new Date (0,0,0, start.getHours(), start.getMinutes(), start.getSeconds());
                 var endTime = new Date (0,0,0, end.getHours(), end.getMinutes(), end.getSeconds());
-                
+                //Note : Issue with data - Sometimes there are entries with no interval. 
+                if (endTime - startTime == 0)
+                    return;
                 if (row.data['state'].localeCompare ('OFF')) {
                     powerCutEvents.push ([momentStart.format('ll'), row.data['state'],'#0F9D58', startTime, endTime]);
                 } else {
@@ -72,33 +75,4 @@ function drawChart() {
             chart.draw(dataTable, options);
 	    }
     });
-    console.log ("Total Items Outside: ", dataTable.getNumberOfRows());
-
-    // dataTable.addRows([
-    //     ['Monday', 'ON ', '#0F9D58', new Date(Date.parse('5/3/2020 12:27:50')), new Date(Date.parse('5/3/2020 13:44:49'))],
-    //     ['Monday', 'OFF', '#DB4437', new Date(Date.parse('5/3/2020 13:44:49')), new Date(Date.parse('5/3/2020 13:48:18'))],
-    //     ['Monday', 'ON ', '#0F9D58', new Date(Date.parse('5/3/2020 13:48:18')), new Date(Date.parse('5/3/2020 15:09:03'))],
-    //     ['Monday', 'OFF', '#DB4437', new Date(Date.parse('5/3/2020 15:09:03')), new Date(Date.parse('5/3/2020 15:14:13'))],
-    //     ['Monday', 'ON ', '#0F9D58', new Date(Date.parse('5/3/2020 15:14:13')), new Date(Date.parse('5/3/2020 17:13:45'))],
-    //     ['Monday', 'OFF', '#DB4437', new Date(Date.parse('5/3/2020 17:13:45')), new Date(Date.parse('5/3/2020 17:18:55'))],
-    //     ['Monday', 'ON ', '#0F9D58', new Date(Date.parse('5/3/2020 17:18:55')), new Date(Date.parse('5/3/2020 18:30:44'))],
-    //     ['Monday', 'OFF', '#DB4437', new Date(Date.parse('5/3/2020 18:30:44')), new Date(Date.parse('5/3/2020 18:36:51'))],
-    //     ['Monday', 'ON ', '#0F9D58', new Date(Date.parse('5/3/2020 18:36:51')), new Date(Date.parse('5/3/2020 20:24:24'))],
-    //     ['Monday', 'OFF', '#DB4437', new Date(Date.parse('5/3/2020 20:24:24')), new Date(Date.parse('5/3/2020 20:29:34'))],
-    //     ['Monday', 'ON ', '#0F9D58', new Date(Date.parse('5/3/2020 20:29:34')), new Date(Date.parse('5/4/2020 0:14:08'))],
-    //     ['Tuesday', 'OFF', '#DB4437', new Date(Date.parse('5/4/2020 0:14:08')), new Date(Date.parse('5/4/2020 0:19:1'))],
-    //     ['Tuesday', 'ON ', '#0F9D58', new Date(Date.parse('5/4/2020 0:19:18')), new Date(Date.parse('5/4/2020 7:04:0'))],
-    // ]);
-
-
-    var options = {
-        timeline: {
-            showRowLabels: true,
-            colorByRowLabel: true,
-            avoidOverlappingGridLines: true,
-            groupByRowLabel: true
-        },
-    };
-
-    chart.draw(dataTable, options);
 }
